936109257a96dd7abd3035a5fec7074627cf36f2
Yang Liu <ylt5b@mail.missouri.edu>
2019-10-06 19:16:34 -0500

    Update 322. Coin Change.py

--- dp/322. Coin Change.py
+++ dp/322. Coin Change.py
@@ -1,21 +1,19 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        dp = [0] + [float('inf')] * amount
-        #solution 1
-        for coin in coins:
-            for i in range(coin, amount+1):
-                dp[i] = min(dp[i], dp[i-coin] + 1)
-                
-        # solution 2
-#         for i in range(1, amount + 1):
-#             for coin in coins:
-#                 if i - coin >= 0:
-#                     dp[i] = min(dp[i], dp[i-coin] + 1)
-      
-        return dp[-1] if dp[-1] != float('inf') else -1
+# Solution 1. DP with bottom up
+    # 1. coins outer loop
+    class Solution(object):
+        def coinChange(self, coins, amount):
+            dp = [0] + [float('inf')] * amount
+            for coin in coins:
+                for i in range(coin, amount+1):
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+            return dp[-1] if dp[-1] != float('inf') else -1
+        
+     # 2. amount outer loop
+     class Solution(object):
+        def coinChange(self, coins, amount):
+            for i in range(1, amount + 1):
+                for coin in coins:
+                    if i - coin >= 0:
+                        dp[i] = min(dp[i], dp[i-coin] + 1)
+            return dp[-1] if dp[-1] != float('inf') else -1
     
