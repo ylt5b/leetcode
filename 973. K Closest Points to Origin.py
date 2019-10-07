c206547b3147da1c90237aaa6cc30433c1f311c7
Yang Liu <ylt5b@mail.missouri.edu>
2019-10-06 22:03:05 -0500

    init commit

---
+++ 973. K Closest Points to Origin.py
@@ -0,0 +1,17 @@
+import heapq
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        heap = []
+        
+        for (x, y) in points:
+            dist = -(x **2 + y **2)
+            if len(heap) == K:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+        return [(x, y) for (dist, x, y) in heap]
