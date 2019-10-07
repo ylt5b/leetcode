4ef9c80c9e88ff3b3b4acdce75957042f5f5f239
Yang Liu <ylt5b@mail.missouri.edu>
2019-10-06 19:19:54 -0500

    Update 322. Coin Change.py

--- dp/322. Coin Change.py
+++ dp/322. Coin Change.py
@@ -11,6 +11,7 @@
      # 2. amount outer loop
      class Solution(object):
         def coinChange(self, coins, amount):
+            dp = [0] + [float('inf')] * amount
             for i in range(1, amount + 1):
                 for coin in coins:
                     if i - coin >= 0:
