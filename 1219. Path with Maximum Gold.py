dd164bbfdaf938bdd33b182b135c860f64811ad2
Yang Liu <ylt5b@mail.missouri.edu>
2019-10-07 11:50:09 -0500

    init commit

---
+++ 1219. Path with Maximum Gold.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def getMaximumGold(self, grid):
+        def dfs(i, j, seen):
+            seen[i][j] = 1 
+            mx = 0
+            for (x, y) in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
+                if x >= 0 and y >= 0 and x <= len(grid)-1 and y <= len(grid[0]) - 1 and seen[x][y] == 0 and grid[x][y] != 0:
+                    mx = max(grid[x][y] + dfs(x, y, seen), mx)
+            seen[i][j] = 0
+            return mx
+        
+        
+        m = len(grid)
+        n = len(grid[0])
+        seen = [[0] * n for _ in range(m)]
+        result = 0
+        for i in range(m):
+            for j in range(n):
+                if grid > 0:
+                    result = max(result, grid[i][j] + dfs(i, j, seen))
+                    
+        return result
