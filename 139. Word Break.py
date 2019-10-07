3c9751b9fdfc20deabe640267f6c5a60d39463fc
Yang Liu <ylt5b@mail.missouri.edu>
2019-10-07 18:48:45 -0500

    add BFS and DFS

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -9,3 +9,37 @@ class Solution(object):
                     if s[i:j] in wordDict:
                         dp[j] = True
         return dp[-1]
+
+ # Solution 2: BFS
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        stack = [0]
+        visited = set()
+        while stack:
+            start = stack.pop()
+            if start in visited:
+                continue
+            visited.add(start)
+            for word in wordDict:
+                if s[start:].startswith(word):
+                    if len(word) == len(s[start:]):
+                        return True
+                    stack.append(start + len(word))
+        return False
+    
+  # Solution 3: DFS
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+            def dfs(index, visit):
+            if index == len(s):
+                return True
+            if visit[index] == True:
+                return 
+            visit[index] = True
+            for j in range(index + 1, len(s) + 1):
+                if s[index:j] in wordDict and dfs(j, visit):
+                    return True
+            return False
+      
+        visit = [False] * (len(s) + 1)
+        return dfs(0, visit)
