417c4af88e6a2c0726c47262716b87ae7ae1cd15
Yang Liu <ylt5b@mail.missouri.edu>
2019-10-09 13:22:05 -0500

    DFS with memoization

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -43,3 +43,27 @@ class Solution(object):
       
         visit = [False] * (len(s) + 1)
         return dfs(0, visit)
+
+    # DFS with memoization:
+    class Solution:
+        def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+         def dfs(start:int, visited, memo):
+            if start == len(s):
+                return True
+            if memo[start] != None:
+                return memo[start]
+            if start in visited: 
+                return
+            visited.add(start)
+            for i in range(start+1, len(s)+1):
+                if s[start:i] in wordDict and dfs(i, visited, memo):
+                    memo[start] = True
+                    return memo[start]
+                
+            memo[start] = False
+            return memo[start] 
+        
+         visited = set()
+         memo = [None] * len(s)
+         return dfs(0, visited, memo)
