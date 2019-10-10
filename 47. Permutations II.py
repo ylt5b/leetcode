b749af607bba40a30a754f0a6b2b0da2ec477983
Yang Liu <ylt5b@mail.missouri.edu>
2019-10-09 19:27:58 -0500

    add solution

--- 47. Permutations II.py
+++ 47. Permutations II.py
@@ -14,3 +14,28 @@ class Solution(object):
                 continue
             self.dfs(nums[:i] + nums[i+1:], path + [nums[i]], res)
             
+            
+    # another solution
+    
+   class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+           
+        def dfs(res:List[List[int]], index, cur_res:List[List[int]], seen:set()):
+            if len(cur_res) == len(nums):
+                res.append(cur_res[:])
+                return
+           
+            for i in range(len(nums)):
+                if i in seen: continue 
+                if i > 0 and  i-1  in seen and nums[i] == nums[i-1]: continue  
+                seen.add(i)
+                print (seen)
+                dfs(res, i, cur_res + [nums[i]], seen)
+                seen.remove(i)
+               
+        res = []
+        seen = set()
+        nums.sort()
+        dfs(res, 0, [], seen)
+        return (res)
+            
