fff009e7b8c3955a3dd5ddd63db8e7612f7e3263
Yang Liu <ylt5b@mail.missouri.edu>
2019-10-14 23:28:45 -0500

    add morris

--- 145. Binary Tree Postorder Traversal.py
+++ 145. Binary Tree Postorder Traversal.py
@@ -29,3 +29,25 @@ class Solution:
         self.dfs(root.left, res)
         self.dfs(root.right, res)
         res.append(root.val)
+
+# solution 3: morris
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        node, res = root, []
+        while node:
+            if not node.right:
+                res.append(node.val)
+                node = node.left
+            else:
+                precessor = node.right
+                while precessor.left and precessor.left != node:
+                    precessor = precessor.left
+                if not precessor.left:
+                    res.append(node.val)
+                    precessor.left = node
+                    node = node.right
+                else:
+                    # precessor.left = None
+                    node = node.left
+                    
+        return res[::-1]
