1e351547eef30fba469325ea2faf2b9d95e04223
Yang Liu <ylt5b@mail.missouri.edu>
2019-12-04 22:54:30 -0600

    init

---
+++ 621. Task Scheduler.py
@@ -0,0 +1,25 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        
+        count = collections.Counter(tasks)
+        heap = []
+        n += 1
+        for key in count.keys():
+            heapq.heappush(heap, -count[key])
+        ans = 0
+        while heap:
+            stack = []
+            count = 0
+            for _ in range(n):
+                if heap:
+                    c = heapq.heappop(heap)
+                    count += 1
+                    if c < -1:
+                        stack.append(c+1)
+            for i in stack:
+                heapq.heappush(heap, i)
+            if heap:
+                ans += n
+            else:
+                ans += count
+        return ans
