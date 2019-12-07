0483596732a21f1e7850e24b0c608434afa2f416
Yang Liu <ylt5b@mail.missouri.edu>
2019-12-07 02:47:06 -0600

    init

---
+++ 399. Evaluate Division.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def calcEquation(self, equations, values, queries):
+        """
+        :type equations: List[List[str]]
+        :type values: List[float]
+        :type queries: List[List[str]]
+        :rtype: List[float]
+        """
+        # 1. dfs
+        graph = collections.defaultdict(list)
+        for equation, value in zip(equations, values):
+            x, y = equation
+            if x not in graph:
+                graph[x] = [(y, value)]
+            else:
+                graph[x].append((y, value))
+            if y not in graph:
+                graph[y] = [(x, 1.0 / value)]
+            else:
+                 graph[y].append((x, 1.0 / value))
+        def dfs(start, end, value, res):
+            if start == end and start in graph: 
+                res[0] = value
+                return
+            if start in visited:
+                return
+            visited.add(start)
+            for node in graph[start]:
+                dfs(node[0], end, value * node[1], res)
+                
+        ans = []      
+        for query in queries:
+            if query[0] not in graph or query[1] not in graph: 
+                ans.append(-1.0)
+                continue
+            visited = set()
+            res = [-1.0] 
+            dfs(query[0], query[1], 1.0, res)
+            ans.append(res[0])
+        return ans
