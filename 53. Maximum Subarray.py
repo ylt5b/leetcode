00863677a2fabc89bf0ec1dcfc64a0c9de624e09
Yang Liu <ylt5b@mail.missouri.edu>
2019-12-06 23:14:28 -0600

    init2

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -40,3 +40,11 @@ class Solution(object):
         for i in range(1, len(nums)):
             dp[i] = max(dp[i-1] + nums[i], nums[i])
         return max(dp)
+
+          curr_sum = nums[0]
+        global_sum = curr_sum
+     
+        for i in range(1, len(nums)):
+            curr_sum = max(curr_sum + nums[i], nums[i])
+            global_sum = max(curr_sum, global_sum)
+        return global_sum
