086c791d20228b1dd59fca0abff085d974106163
Yang Liu <ylt5b@mail.missouri.edu>
2019-12-31 00:03:06 -0600

    add bfs

--- 399. Evaluate Division.py
+++ 399. Evaluate Division.py
@@ -38,3 +38,28 @@ class Solution(object):
             dfs(query[0], query[1], 1.0, res)
             ans.append(res[0])
         return ans
+    
+    
+       # bfs
+        graph = collections.defaultdict(list)
+        for equation, value in zip(equations, values):
+            x, y = equation       
+            graph[x].append((y, value))     
+            graph[y].append((x, 1.0 / value))
+            
+        def  bfs(src, dst):
+            if src not in graph or dst not in graph:
+                return -1.0
+            seen = set()
+            queue = collections.deque([(src, 1.0)])
+            while queue:
+                x, value = queue.popleft()
+                if x == dst:
+                    return value
+                seen.add(x)
+                for node, val in graph[x]:
+                    if node not in seen:
+                        seen.add(node)
+                        queue.append((node, value * val))
+            return -1.0
+        return [bfs(s, d) for s, d in queries]
